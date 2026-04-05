b011df57ecb77c28e4dc3088f3ed16bcec44868b
Adrian <36133718+AdrianN17@users.noreply.github.com>
2026-04-05 14:40:44 -0500

    added application config

---
+++ TransactionService/TransactionService.Application/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using TransactionService.Application.Common.Behaviors;
+global using TransactionService.Application.Common.Helpers;
+global using TransactionService.Application.Common.Exceptions;
+global using TransactionService.Domain.Interfaces;
+global using TransactionService.Domain.Entities;
+global using TransactionService.Domain.ValueObjects;
+global using TransactionService.Domain.Enums;
