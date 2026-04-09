3a8afe7302d780fcd1a9af3cdcda2df0d4cae3f8
Adrian <36133718+AdrianN17@users.noreply.github.com>
2026-04-08 23:45:31 -0500

    Fis SendOperation

--- WalletService/WalletService.Infrastructure/Messaging/SendOperation.cs
+++ WalletService/WalletService.Infrastructure/Messaging/SendOperation.cs
@@ -1,5 +1,8 @@
-﻿namespace WalletService.Infrastructure.Messaging;
+﻿using MassTransit;
 
+namespace WalletService.Infrastructure.Messaging;
+
+[MessageUrn("TransactionService.Application.Abstractions.Messaging.Sender:SendOperation")]
 public sealed record SendOperation(
     string Type,
     string WalletId,
